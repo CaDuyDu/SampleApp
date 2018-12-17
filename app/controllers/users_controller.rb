b40a6e2193ffea2fcdcce887573f238baf68153a
CaDuyDu <ducd.kma@gmail.com>
2018-12-17 15:45:32 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".welcome_to_the_sample_app"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+
+    unless @user
+      render json: {
+        error: "User with id #{params[:id]} not found."
+      }, status: :not_found
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
