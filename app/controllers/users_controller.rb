ad4225f489540b52b984c6d857b90ad80c720686
CaDuyDu <ducd.kma@gmail.com>
2018-12-17 15:45:32 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find_by id: params[:id]
+    if !@user
+      render json: {
+        error: "User with id #{params[:id]} not found."
+      }, status: :not_found
+    end
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".welcome_to_the_sample_app"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,7 @@
 module ApplicationHelper
-
-    # Returns the full title on a per-page basis.
-    def full_title(page_title = '')
-      base_title = "Ruby on Rails Tutorial Sample App"
-      if page_title.empty?
-        base_title
-      else
-        page_title + " | " + base_title
-      end
-    end
+  # Returns the full title on a per-page basis.
+  def full_title page_title = ""
+    base_title = t "Ruby_on_Rails_Tutorial_Sample_App"
+    page_title.empty? ? base_title : page_title + " | " + base_title
   end
-
+end
